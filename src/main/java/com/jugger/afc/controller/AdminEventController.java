9f86c53ce4bdaf2e95e56c451469ec8e3d0c2dc4
rahul161616 <rahul1616@gmail.com>
2026-05-10 11:10:09 +0545

    Allow multiple methods for event updates

--- src/main/java/com/jugger/afc/controller/AdminEventController.java
+++ src/main/java/com/jugger/afc/controller/AdminEventController.java
@@ -10,8 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,7 +34,7 @@ public class AdminEventController {
         return ResponseEntity.ok(eventInterestService.getEventInterests(eventId));
     }
 
-    @PutMapping("/{eventId}")
+    @RequestMapping(value = "/{eventId}", method = {RequestMethod.PUT, RequestMethod.PATCH, RequestMethod.POST})
     public ResponseEntity<AddEventsAdminResponse> updateEvent(
             @PathVariable UUID eventId,
             @RequestBody AddEventsAdminRequest request
